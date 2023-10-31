96f4cd7e98625aed3c5140018dee15bb844bd668
Raul Domingues <99521069+Raulhdomingues@users.noreply.github.com>
2023-10-31 09:21:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const botoes = document.querySelectorAll('button');
 const firstName = document.querySelector('.nome');
-const lastName = document.querySelector('sobrenome');
+const lastName = document.querySelector('.sobrenome');
 const email = document.querySelector('.email');
-const senha = document.querySelector('.senha').value;
+const senha = document.querySelector('.senha');
 const inputs = document.querySelectorAll('input');
 
 function enviado() {
@@ -14,20 +14,21 @@ function enviado() {
         }
     });
 
+
     if (vazio) {
         alert('Por favor, preencha todos os campos!');
     }
-
-    if(firstName.value == '' || lastName.value == '' || email.value == '') {
-        alert('Porfavor, Preencha os campos!')
-    }
-
-
-    if(![a-z].test(senha) && ![A-Z].test(senha) && ![0-9].test(senha)){
-        alert('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.')
-    } else if(!/[@#$%^&+=]/.test(senha)) {
-        alert('A senha deve conter caracteres especiais!')
-    }
 }
 
-
+const formulario = document.querySelector('#formulario');
+formulario.addEventListener('submit', (e) => {
+    
+    if (!/[a-z]/.test(senha.value) || !/[A-Z]/.test(senha.value) || !/[0-9]/.test(senha.value)) {
+        alert('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.');
+    } else if (!/[@#$%^&+=]/.test(senha.value)) {
+        alert('A senha deve conter caracteres especiais!');
+    }
+    
+    e.preventDefault();
+})
+    
